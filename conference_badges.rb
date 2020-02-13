c162337f4c43d83598b86f76fadb57fa050f9a5d
Abbas Parpia <aparpia03@gmail.com>
2020-02-13 22:24:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ def assign_rooms(speakers)
   speakers.each_with_index do |speaker,index|
    room_numbers.push("Hello, #{speaker}! You'll be assigned to room #{index+1}!")
   end
-     
+     binding.pry
     room_numbers
 end
 
